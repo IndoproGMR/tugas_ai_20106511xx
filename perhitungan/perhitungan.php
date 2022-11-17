6372282b8c9b34baacd7430f88034b689b95d474
Achmad Naji IndoproGMR <indoprogamer007@gmail.com>
2022-11-17 15:45:46 +0700

    php update

--- perhitungan/perhitungan.php
+++ perhitungan/perhitungan.php
@@ -5,3 +5,10 @@ function hitung($x, $y)
 {
     return ($x + $y);
 }
+
+echo "test";
+// echo $_POST["layer"];
+// echo $_POST["jenis_mobil"];
+
+// echo $_GET["layer"];
+// echo $_GET["jenis_mobil"];
